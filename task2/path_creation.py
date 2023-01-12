55312ce8623829d3e41753e2f8fabe307928aaa9
JanEttrich <Jan.Ettrich00@gmx.de>
2023-01-12 15:46:03 +0100

    Added query for path distances

--- task2/path_creation.py
+++ task2/path_creation.py
@@ -13,11 +13,20 @@ def run_query(query, params):
 
 
 query_create_path_nodes = """
-    MATCH len=(j:Junction)-[:CONSISTS_OF]->(n:Node{time:$time})-[:SEGMENT*{is_junction:$is_junction}]->(m:Node)<-[:CONSISTS_OF]-(i:Junction), 
-    (n)<-[:CONSISTS_OF]-(l:Loop)-[:CONSISTS_OF]->(m) 
-    MERGE (n)<-[:IN_PATH]-(p:Path{time:n.time,id:toString(n.id)+"-"+toString(m.id), path_length:length(len)-2})-[:IN_PATH]->(m)
+    MATCH len=(j:Junction)-[:CONSISTS_OF]->(n:Node)-[:SEGMENT*{is_junction:$is_junction}]->(m:Node)<-[:CONSISTS_OF]-(i:Junction), 
+    (n)<-[:CONSISTS_OF]-(l:Loop)-[:CONSISTS_OF]->(m), 
+    (j)-[:CONSISTS_OF]->(:Node)--(l)--(:Node)<-[:CONSISTS_OF]-(i)
+    MERGE (n)<-[:STARTS_WITH]-(p:Path{time:n.time,start_junc:j.global_id, end_junc:i.global_id, path_length:length(len)-2, loop_id:l.id})-[:ENDS_WITH]->(m)
     """
 
-params = {"is_junction": False, "time": 50}
-result = run_query(query_create_path_nodes, params)
+query_add_distance_to_paths = """
+    MATCH (p:Path)-[:STARTS_WITH]->(n:Node), 
+    relations=(n)-[:SEGMENT*{is_junction:$is_junction}]->(m:Node), 
+    (m)<-[:ENDS_WITH]-(p) 
+    SET p.distance=reduce(distance=0, r in relationships(relations) | distance + r.distance)
+    """
+
+params = {"is_junction": False}
+run_query(query_create_path_nodes, params)
+run_query(query_add_distance_to_paths, params)
 driver.close()
